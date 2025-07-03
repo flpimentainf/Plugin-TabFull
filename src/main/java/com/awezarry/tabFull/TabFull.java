910959ef1e30d2e3c5d77ef76cb8871a0f340798
flpimentainf <francisco.fta90@gmail.com>
2025-07-03 00:27:52 -0300

    Version(0.0.1) Colocado Header e Footer, Nome do Player no Tab, Quantas Pessoas Online.

--- src/main/java/com/awezarry/tabFull/TabFull.java
+++ src/main/java/com/awezarry/tabFull/TabFull.java
@@ -1,17 +1,28 @@
 package com.awezarry.tabFull;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TabFull extends JavaPlugin {
+public final class TabFull extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+        e.getPlayer().setPlayerListHeaderFooter(ChatColor.GOLD + "" + ChatColor.BOLD +"§6NIGHTFALL\n§7Servidor Survival",
+                "§eSite: nightfall.com\n§fDiscord: discord.gg/nightfall");
+
+        Player player = e.getPlayer();
+        String header = ChatColor.AQUA + "Bem-vindo " + player.getName();
+        String footer = ChatColor.GRAY + "Online: " + Bukkit.getOnlinePlayers().size();
+        player.setPlayerListHeaderFooter(header, footer);
     }
 }
